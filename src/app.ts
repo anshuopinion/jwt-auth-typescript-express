c9b5532cc7beb5f2fc53482004aa74383e301cc9
Anshu Raj <anshusopinion@gmail.com>
2021-11-21 07:43:23 +0530

    verify mail done

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,15 @@ import userRoute from "./routes/userRoutes";
 import mongoose from "mongoose";
 import { DB, PORT } from "./config";
 import { errorHandler } from "./middleware/errorHanlder";
-import morgan from "morgan";
 import passport from "passport";
 import kPassport from "./middleware/passport";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(passport.initialize());
 kPassport(passport);
 
